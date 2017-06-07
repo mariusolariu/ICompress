4940df0bbe8175f2f34fd86c073f7af1e5930210
RonanGui <ronan.guillaume@etu.univ-tours.fr>
2017-06-07 17:59:06 +0200

    ClientConnection update

--- core/src/com/mygdx/icompress/ClientConnection.java
+++ core/src/com/mygdx/icompress/ClientConnection.java
@@ -1,11 +1,7 @@
 package com.mygdx.icompress;
 
-import com.mygdx.archiveAlgorithm.Zip;
-import com.mygdx.server.ZipObject;
-
 import java.io.*;
 import java.net.Socket;
-import java.util.Random;
 
 /**
  * Created by Ronan
@@ -13,14 +9,16 @@ import java.util.Random;
  */
 public class ClientConnection implements Runnable{
     private Socket connection = null;
-    private ObjectOutputStream writer = null;
-    private ObjectInputStream reader = null;
-    private ZipObject zipObject;
+    private OutputStream writer = null;
+    private InputStream reader = null;
+    private File inputFile;
+    private File outputFile;
 
-    public ClientConnection(String host, int port, ZipObject zipObject){
-        this.zipObject = zipObject;
+    public ClientConnection(String host, int port, File inputFile, File outputFile){
         try {
             connection = new Socket(host, port);
+            this.inputFile = inputFile;
+            this.outputFile = outputFile;
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -29,23 +27,57 @@ public class ClientConnection implements Runnable{
 
     public void run(){
         try {
-            writer = new ObjectOutputStream(connection.getOutputStream());
-            reader = new ObjectInputStream(connection.getInputStream());
+            writer = connection.getOutputStream();
+            reader = connection.getInputStream();
+            byte[] bytes = new byte[16 * 1024];
+
+
 
             //We send the request to the server
-            writer.writeObject(zipObject);
-            //We use flush to let the flux open
-            writer.flush();
+            byte[] mybytearray = new byte[(int) inputFile.length()];
 
+            FileInputStream fis = new FileInputStream(inputFile);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            DataInputStream dis = new DataInputStream(bis);
+            dis.readFully(mybytearray, 0, mybytearray.length);
+
+            OutputStream os = connection.getOutputStream();
+
+            //Sending file name and file size to the server
+            DataOutputStream dos = new DataOutputStream(os);
+            dos.writeLong(mybytearray.length);
+            dos.write(mybytearray, 0, mybytearray.length);
+            dos.flush();
+
+            //Sending file data to the server
+            os.flush();
+            System.out.println("I will send");
+            int readLength;
+            while ((readLength = bis.read(bytes)) > 0) {
+                os.write(bytes, 0, readLength);
+            }
+            os.flush();
             System.out.println("Order sent to the server");
 
-            //We are waiting the reply
-            ZipObject response = read();
-            System.out.println("\t * Reply received " + response);
-
-            // TODO: 05/06/2017 When the app receives the server's answer
+            //Save the data sent by the server
+            //We create the new file where we will save the data
+            InputStream in = connection.getInputStream();
+            ObjectInputStream clientData = new ObjectInputStream(in);
+            int bytesRead;
+            OutputStream output = new FileOutputStream(outputFile);
+            long size = clientData.readLong();
+            System.out.println("read: " + size);
+            byte[] buffer = new byte[1024];
+            while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1)
+            {
+                output.write(buffer, 0, bytesRead);
+                size -= bytesRead;
+            }
+            output.flush();
 
         } catch (IOException e1) {
+            System.out.println("error !!!");
             e1.printStackTrace();
         }
         try {
@@ -54,16 +86,5 @@ public class ClientConnection implements Runnable{
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    //Method to read the server's reply
-    private ZipObject read() throws IOException{
-        ZipObject result = null;
-        try {
-            result = (ZipObject)reader.readObject();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return result;
     }
 }
